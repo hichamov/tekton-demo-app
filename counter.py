3f1624327da4611f6d5fa870eaa3e14c799ecc63
hichamov <mr.farhi.hicham@gmail.com>
2021-04-26 14:24:50 +0000

    edit counter py

---
+++ counter.py
@@ -0,0 +1,20 @@
+import http.server
+import random
+from prometheus_client import start_http_server
+from prometheus_client import Counter
+
+# Adding counters of requests and exceptions
+REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
+#EXCEPTIONS = Counter('hello_world_exceptions_total', 'Exceptions serving Hello World.')
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+        def do_GET(self):
+            REQUESTS.inc()
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Test From Tekton !!!")
+
+if __name__ == "__main__":
+        start_http_server(8000)
+        server = http.server.HTTPServer(('0.0.0.0', 8001), MyHandler)
+        server.serve_forever()
